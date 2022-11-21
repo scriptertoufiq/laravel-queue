9e243a6e0b99815d97fcdfaa32cbc3ecddb355d4
scriptertoufiq <scriptertoufiq@gmail.com>
2022-11-21 16:04:37 +0600

    Project Init

---
+++ app/Http/Controllers/ProblemTwoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProblemTwoController extends Controller
+{
+    public function form()
+    {
+        return view('form');
+    }
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form</title>
+</head>
+
+<body>
+    <form action="">
+        <label for="name">Name</label>
+        <input type="text" name="name">
+        <br>
+        <label for="email">Email</label>
+        <input type="email" name="email">
+        <br>
+        <label for="message">Message</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <br>
+        <br>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+</html>
